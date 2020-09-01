a9b53f406819c5c3b0defe63bf73e7bd2d3f7c77
nakhun9279 <fjfj9279@naver.com>
2020-09-01 20:21:00 +0900

    18.2.1 미들웨어 만들기

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 
 
@@ -18,7 +19,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-
-
 serviceWorker.unregister();
