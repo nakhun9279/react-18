24c46380cff535701d41617a81d4fde4d4190aea
nakhun9279 <fjfj9279@naver.com>
2020-09-01 21:22:22 +0900

    18.3.2.3 API요청상태 관리하기

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import counter, { counterSaga } from './counter';
-import sample from './sample';
+import sample, { sampleSaga } from './sample';
 import loading from './loading';
 const rootReducer = combineReducers({
   counter,
@@ -11,7 +11,7 @@ const rootReducer = combineReducers({
 
 export function* rootSaga() {
   // all 함수는 여러 사가를 합쳐 주는 역할을 합니다.
-  yield all([counterSaga()]);
+  yield all([counterSaga(), sampleSaga()]);
 }
 
 
